c6d77196a0bef366a4d9d09f97fcfac07eac40c7
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-10-28 03:44:57 +0200

    Add docstrings

--- mail.py
+++ mail.py
@@ -22,6 +22,11 @@ def get_file_path(profile=PROFILE, folder=FOLDER):
 DATE_FORMAT = "%a %b %d %X %Y"
 
 def grab_one_mail(queue, date_format=DATE_FORMAT):
+    """Format two elments of a queue to a mail namedtuple
+
+    :param deque queue: the queue
+    :param str date_format: the date format specifier
+    """
     date_string = queue.popleft()
     if date_format:
         date = datetime.strptime(date_string, date_format)
@@ -32,6 +37,20 @@ def grab_one_mail(queue, date_format=DATE_FORMAT):
 
 _mail = namedtuple("mail", ['date', 'content'])
 def iter_mails(string, regex=RE_MSG_SPLIT, options=None):
+    """Split a string with a regex and iterate over the chunks.
+
+    The regex is supposed to have one capture group, which makes
+    ``re.split`` return information in chunks of two list elements.
+    Now, ``grab_one_mail`` is used to yield the chunk.
+
+    :param str string: The string containing the mails
+    :param re regex: The regex to apply.  Must have one capture group.
+    :param dict options: A dict being passed to ``grab_one_mail`` as
+        kwargs
+
+    :return: An iterator over the chunks as a _mail namedtuple
+    :rtype: iterator[_mail]
+    """
     if options is None:
         options = {}
 
